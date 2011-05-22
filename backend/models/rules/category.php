4f486bb3c99436080b4e3ca0d9718dbd13b8087c
Alexandros Dallas <alexd3499@gmail.com>
2011-05-22 13:43:21 +0300

    validation rules

---
+++ backend/models/rules/category.php
@@ -0,0 +1,31 @@
+<?php
+/*------------------------------------------------------------------------
+# com_ask - Ask (Questions)
+# ------------------------------------------------------------------------
+# @author    Alexandros D
+# @copyright Copyright (C) 2011 Alexandros D. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# @Website: http://alexd.mplofa.com
+-------------------------------------------------------------------------*/
+
+//No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla formrule library
+jimport('joomla.form.formrule');
+ 
+/**
+ * Form Rule class for the Joomla Framework.
+ */
+class JFormRuleCategory extends JFormRule
+{
+	/**
+	 * The regular expression.
+	 *
+	 * @access	protected
+	 * @var		string
+	 * @since	1.6
+	 */
+	protected $regex = '^[^0].+$'; //Should not be == 0 (no category)
+}
+
